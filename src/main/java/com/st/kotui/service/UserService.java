2ac2bfcce3dc1bd19f5e05ad68b17ed1ab12b2b1
Veselin Karaganev <Veselin Karaganev>
2016-04-05 11:50:33 +0300

    [KOT-UI] fixed UserService
    Signed-off-by: Veselin Karaganev <Veselin Karaganev>

--- src/main/java/com/st/kotui/service/UserService.java
+++ src/main/java/com/st/kotui/service/UserService.java
@@ -7,14 +7,16 @@ import com.st.kotui.db.Persistence;
 public class UserService {
 
 	public JSONObject getUser(long id) {
-		JSONObject jo = Persistence.get().
-		return jo;
+		/*JSONObject jo = Persistence.get().
+		return jo;*/
+		return null;
 	}
 	
 	public JSONObject addUser(String username) {
-		JSONObject existing = Persistence.get().getUser(username);
+		/*JSONObject existing = Persistence.get().getUser(username);
 		//if (existing == )
-		return jo;
+		return jo;*/
+		return null;
 	}
 
 	public JSONObject getResources() {
